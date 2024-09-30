0b637e4ffe80ffaea32c4a32be3a391a9aed9ac3
danielhmahecha <dh.mahecha25@uniandes.edu.co>
2024-09-30 18:41:53 -0500

    actualizo repositorios
    productos, ordenes, proveedores

---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/OrdenesRepository.java
@@ -0,0 +1,34 @@
+package uniandes.edu.co.proyecto.repositorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import uniandes.edu.co.proyecto.modelo.Ordenes;
+
+import java.sql.Date;
+import java.util.Collection;
+
+public interface OrdenesRepository  extends JpaRepository<Ordenes, Integer>{
+
+    @Query(value = "SELECT * FROM ordenes", nativeQuery = true)
+    Collection<Ordenes> darOrdenes();
+
+    @Query(value = "SELECT * FROM ordenes WHERE id = :id, fecha_esperada = :fecha_esperada, estado = :estado, proveedor_nit = :proveedor_nit, sucursal_id = :sucursal_id, cantidad = :cantidad_id, precio = :precio, recepcion_id = :recepcion_id", nativeQuery = true)
+    Ordenes darOrdenesPorId(@Param("id") Integer id, @Param("fecha esperada") Date fecha_esperada, @Param("estado") String estado, @Param("proveedor_nit") Integer proveedor_nit, @Param("sucursal_id") Integer sucursal_id, @Param("cantidad") Integer cantidad, @Param("precio") Integer precio, @Param("recepcion_id") Integer recepcion_id);
+
+    @Query(value = "SELECT id FROM ordenes WHERE fecha_esperada = :fecha_esperada, estado = :estado, proveedor_nit = :proveedor_nit, sucursal_id = :sucursal_id, cantidad = :cantidad_id, precio = :precio, recepcion_id = :recepcion_id", nativeQuery = true)
+    Ordenes darOrdenesId(@Param("fecha esperada") Date fecha_esperada, @Param("estado") String estado, @Param("proveedor_nit") Integer proveedor_nit, @Param("sucursal_id") Integer sucursal_id, @Param("cantidad") Integer cantidad, @Param("precio") Integer precio, @Param("recepcion_id") Integer recepcion_id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Ordenes SET estado = 'anulada' WHERE id = :id", nativeQuery = true)
+    void actualizarOrdenes(@Param("id") Integer id);
+
+    @Modifying
+    @Transactional //que hacer con recepcion
+    @Query(value = "INSERT INTO Ordenes (fecha_esperada, estado, proveedor_nit, sucursal_id, cantidad, precio) VALUES (:fecha_esperada, 'vigente', :proveedor_nit, :sucursal_id, :cantidad, :precio)", nativeQuery = true)
+    void insertarOrdenes(@Param("id") Integer id, @Param("fecha esperada") Date fecha_esperada, @Param("proveedor_nit") Integer proveedor_nit, @Param("sucursal_id") Integer sucursal_id, @Param("cantidad") Integer cantidad, @Param("precio") Integer precio, @Param("recepcion_id") Integer recepcion_id);
+
+}
