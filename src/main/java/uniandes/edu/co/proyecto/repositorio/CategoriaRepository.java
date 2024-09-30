bad8b57bf67d7217e8351ea6427ed4f1aada0934
andreschaparr0 <141948128+andreschaparr0@users.noreply.github.com>
2024-09-30 15:53:42 -0500

    Update CategoriaRepository.java

--- src/main/java/uniandes/edu/co/proyecto/repositorio/CategoriaRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/CategoriaRepository.java
@@ -10,10 +10,10 @@ import uniandes.edu.co.proyecto.modelo.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
-    @Query(value = "SELECT * FROM categoria", nativeQuery = true)
+    @Query(value = "SELECT * FROM categorias", nativeQuery = true)
     Collection<Categoria> darCategoria();
 
-    @Query(value = "SELECT * FROM categoria WHERE codigo = :codigo", nativeQuery = true)
+    @Query(value = "SELECT * FROM categorias WHERE codigo = :codigo", nativeQuery = true)
     Categoria darCategoriaPorCodigo(@Param("codigo") Integer codigo);
 
     @Query(value = "INSERT INTO categorias (nombre, descripcion, almacenamiento) VALUES (:nombre, :descripcion, :almacenamiento)", nativeQuery = true)
